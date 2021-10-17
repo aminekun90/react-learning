3170fbd8ac3a0521e820e8121774fcc4b02db429
aminekun90 <mohamed.amine.bouzahar@gmail.com>
2021-10-18 01:48:33 +0200

    Props and separate component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
+import Todo from "./component/Todo";
 function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <div className="card">
-        <h2>TITLE</h2>
-        <div className="actions">
-          <button className="btn">Delete</button>
-        </div>
-      </div>
+      <Todo text="Learn React js" />
+      <Todo text="Master React js" />
+      <Todo text="Expand knowlege about React js" />
     </div>
   );
 }
